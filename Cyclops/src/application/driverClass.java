dcf326dc55b62457078fe6a5034f18831f97a8f5
HugoGarza833 <31325884+HugoGarza833@users.noreply.github.com>
2017-11-22 11:31:44 -0600

    Add files via upload

--- Cyclops/src/application/driverClass.java
+++ Cyclops/src/application/driverClass.java
@@ -1,4 +1,7 @@
 package application;
+<<<<<<< HEAD
+import java.io.File;
+=======
 
 import javafx.application.*;
 import javafx.scene.paint.*;
@@ -12,10 +15,212 @@ import javafx.scene.image.ImageView;
 import javafx.scene.Group;
 import javafx.scene.input.*;
 
+>>>>>>> 1333257434d2cdb851ca5d46fa258c46f1d9f549
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
+<<<<<<< HEAD
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class driverClass extends Application {
+	//////////////////////////////////////////
+	//           Alysas code                //
+	protected final double	screenX = 800;
+	protected final double	screenY = 600;
+	protected ArrayList<Enemy> enemyList;
+	protected Group enemies;
+	protected double startingSpawnRate = 1;
+	protected double currentRate = startingSpawnRate;
+	protected double rateProgression = 0.995;
+	protected int enemyMaxAngle = 15;
+	protected Random myRandom = new Random();
+	protected AnimationTimer spawnTime;
+	private AnimationTimer gameLoop;
+	/////////////////////////////////////////////
+	protected Scene scene;
+	protected Stage mainStage;
+	protected Group root;
+	public final static String 	BACKGROUND_IMG = "resources/space2.gif";
+	protected ImageView imgBackground;
+    boolean goNorth, goSouth;
+	static boolean shoot;
+    private static Image warrior;
+    static Node  node;
+    
+    
+    
+public static void main(String[] args) {
+	launch(args);
+}
+		
+
+
+public void start(Stage firstStage) {
+		//////////////////////////////////
+		//Alyssas code
+		enemyList = new ArrayList<Enemy>();
+		enemies = new Group();
+		/////////////////////////
+		
+		
+		
+		
+		
+		//declaring/setting  all the pictures and images used
+	    warrior = new Image("resources/warrior2.png");
+	    node = new ImageView(warrior);
+	    ((ImageView) node).setFitHeight(75);
+	    ((ImageView) node).setFitWidth(75);
+		imgBackground = new ImageView(new Image(getClass().getClassLoader().getResource(BACKGROUND_IMG).toString()));
+		imgBackground.setFitHeight(850);
+		imgBackground.setFitWidth(1000);	
+		
+       
+		
+        
+        //declaring group
+        Group root = new Group();
+		
+		
+		//adding the pictures to the scene
+        root.getChildren().add(imgBackground);
+		root.getChildren().add(node);
+		root.getChildren().add(enemies);
+		//bullet is added inside the if space == true statement
+		
+	
+		
+		Stage mainStage = firstStage;
+		mainStage.setTitle("Starclops");
+		
+		//set the player at the middle of the screen.
+		Player.movePlayerTo(50, 350);
+		//plays the long background music.
+		play(0);
+		Scene scene = new Scene(root, 1000, 700, Color.BLACK);
+		
+		//playerControlls
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @SuppressWarnings("incomplete-switch")
+			@Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                case UP:    goNorth = true; break;
+                case DOWN:  goSouth = true; break;
+                case SPACE:  shoot = true; break;
+                }
+            }
+        });
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @SuppressWarnings("incomplete-switch")
+			@Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                case UP:    goNorth = false; break;
+                case DOWN:  goSouth = false; break;
+                case SPACE: shoot = false;   break;
+
+                }
+            }
+        });
+		
+		spawnSprites();
+		//starts the show i believe
+		 mainStage.setScene(scene);
+	     mainStage.show();
+	     StartGameLoop();
+	     
+	     //stops music when the windows is closed, otherwise the music will continue playing
+	     mainStage.setOnCloseRequest(e -> {
+	         Platform.exit();
+	         System.exit(0);
+	     });
+	     
+        AnimationTimer timer = new AnimationTimer() {
+        	           
+            public void handle(long now) {
+                //if statements that tells how much to move up and down
+            	int dx = 0, dy = 0;
+                if (goNorth) dy -= 10; //how fast the player moves north
+                if (goSouth) dy += 10; //how fast the player moves south
+                
+                //method call to move player
+                Player.movePlayerBy(dx, dy); 
+                
+                //everything that is inside the if statement is how the bullet is made
+               if (shoot == true){
+            	   final Rectangle rectBasicTimeline = new Rectangle(node.getLayoutX()+70,node.getLayoutY()+30 , 70, 10);//location and size of bullet
+             		rectBasicTimeline.setFill(Color.RED);
+             		final Timeline timeline = new Timeline();
+             		timeline.setCycleCount(1);
+             		timeline.setAutoReverse(false);
+             		
+             		//you can change speed by moving which ever number of the next two lines
+             		final KeyValue kv = new KeyValue(rectBasicTimeline.xProperty(),6000);
+             		final KeyFrame kf = new KeyFrame(Duration.millis(6000), kv);
+             		root.getChildren().add(rectBasicTimeline);
+             		
+             		//play the bullet sound effect once
+             		playShot(1, 1);
+             		
+             		timeline.getKeyFrames().add(kf);
+             		timeline.play();
+             		//so it wont be shooting like a machine gun. even if you press space once it will read it more than once.
+             		shoot = false;
+             		
+             		
+             		//still need to find a way to block the user from leaving SPACE pressed. otherwise it will shoot like a machine gun.
+             		//also delete the bullet when it gets to more than 1050 in the x axis to save space
+                }
+               
+            }
+           
+		};
+		timer.start();
+		gameLoop.start();
+    }
+
+
+public void reset_game() {
+	currentRate = startingSpawnRate;
+	clearSprites();
+}
+
+public void clearSprites() {
+	for (Iterator<Enemy> enemyIterator = enemyList.iterator(); enemyIterator.hasNext();) {
+		Enemy enemyNode = enemyIterator.next();
+		enemies.getChildren().remove(enemyNode);
+		enemyIterator.remove();
+	}
+}
+
+
+
+private void spawnSprites() {
+	 AnimationTimer spawnTime = new AnimationTimer() {
+		long lastSpawn = 0;
+		int randomInt;
+=======
 import javafx.animation.*;
 import javafx.scene.Scene;
 
@@ -113,6 +318,7 @@ public class driverClass extends Application {
 			long lastSpawn = 0;
 			int randomInt;
 		@Override
+>>>>>>> 1333257434d2cdb851ca5d46fa258c46f1d9f549
 		public void handle(long timestamp) {
 			if ((timestamp - lastSpawn >= currentRate * (long)1000000000)) {
 				currentRate *= rateProgression;
@@ -131,9 +337,15 @@ public class driverClass extends Application {
 			}
 		};
 		spawnTime.start();
+<<<<<<< HEAD
+		
+	}
+private void StartGameLoop() {
+=======
 	}
 	
 	private void StartGameLoop() {
+>>>>>>> 1333257434d2cdb851ca5d46fa258c46f1d9f549
 	gameLoop = new AnimationTimer() {
 		@Override
 		public void handle(long l) {
@@ -148,4 +360,83 @@ public class driverClass extends Application {
 		
 		gameLoop.start();
 	}
+<<<<<<< HEAD
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//background music
+public void play( int delay) {
+	for (int i = 0; i < 1; i++) {
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					File file = new File("Fantasy_Game_Background1.wav");
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(file));
+					clip.start();
+					Thread.sleep(clip.getMicrosecondLength());
+
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}.start();
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
+//laser music event
+public static void playShot(int delay, int numberOfLoops) {
+	for (int i = 0; i < numberOfLoops; i++) {
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					File file = new File("laser7.wav");
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(file));
+					clip.start();
+					Thread.sleep(clip.getMicrosecondLength());
+
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}.start();
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+	}
+=======
+}
+>>>>>>> 1333257434d2cdb851ca5d46fa258c46f1d9f549
